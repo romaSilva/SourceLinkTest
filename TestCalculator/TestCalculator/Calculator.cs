16c25993d0343ca0fd198bade3decf9953dac075
Romário da Silva <romario.dasilva@pris.com.br>
2020-12-11 15:32:02 -0300

    teste 1

---
+++ TestCalculator/TestCalculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestCalculator
+{
+    public class Calculator
+    {
+
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int SubtractNumbers(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
